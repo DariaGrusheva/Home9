7c02cb2f78da6371ee7eb2b0b5e4549bad404614
Daria <gdaria1609@yandex.ru>
2024-05-07 22:58:22 +0500

    Добавьте файлы проекта.

---
+++ Home9/HomeWork.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9
+{
+    // Дописать приложение поиска файла используя многопоточность
+
+    internal class HomeWork
+    {
+        /// <summary>
+        /// Поиск слов в файлах с использование многопоточности
+        /// </summary>
+        /// <param name="filePaths">Список имен файлов</param>
+        /// <param name="word">Искомое слово</param>
+        /// <returns></returns>
+        public static List<string> FindWordInMultiThread(List<string> filePaths, string word)
+        {
+            List<string> result = new List<string>();
+            var threads = new List<Thread>();
+            foreach (var path in filePaths)
+            {
+                var thread = new Thread(() =>
+                {
+                    string content = File.ReadAllText(path);
+                    if (content.Contains(word))
+                        result.Add(path);
+                });
+                threads.Add(thread);
+                thread.Start();
+            }
+            foreach (var thread in threads) thread.Join();
+            return result;
+        }
+
+        /// <summary>
+        /// Поиск имени файла
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static bool FindWord(string path, string word)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var tempStr = sr.ReadLine();
+                    if (tempStr.Contains(word))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+        }
+
+
+        /// <summary>
+        /// Перебор файлов по заданному пути с заданным и добавлением в список имен файлов
+        /// </summary>
+        /// <param name="path">Путь к файлу</param>
+        /// <param name="fileExtension"> Расширение котрое ищем</param>
+        /// <param name="list">Пополняемый список</param>
+        public static void FindFiles(string path, string fileExtension, List<string> list)
+        {
+            list.AddRange(Directory.GetFiles(path, fileExtension));
+
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                FindFiles(dir, fileExtension, list);
+            }
+        }
+
+    }
+}
---
+++ Home9/Program.cs
@@ -0,0 +1,312 @@
+﻿using System.IO;
+using System.Threading;
+//Дописать приложение поиска файла используя многопоточность
+namespace Home9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            HomeWork.FindFiles("D:\\projects\\example-generator\\Output", "*.txt", list);
+
+            var findLit2 = HomeWork.FindWordInMultiThread(list, "comment:447a2fef-13db-4b73-9b04-c5eb1743ae59");
+            foreach (string word in findLit2)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        /*static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.WriteLine($"Start");
+
+            Thread thread = new Thread(Print);
+            thread.Start();
+            //Примеры создания потоков
+            //Thread thread = new Thread(new ThreadStart(Print));
+            //Thread thread = new Thread(() => Console.Writeline($"Thread"));
+            //Thread thread = new Thread(() =>
+            //{
+            //Console.Writeline($"Thread")
+            //});
+            Thread.Sleep(3000);
+            Console.WriteLine($"Main Thread");
+        }
+
+        public static void Print()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine($"Second Thread");
+        }*/
+
+
+        //синхронизация
+        /*static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.WriteLine($"Start");
+
+            Thread thread = new Thread(Print);
+            thread.Start();
+           
+            thread.Join();//приостанавливает текущий поток
+            Console.WriteLine($"Main End");
+        }
+
+        public static void Print()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine($"Second Thread");
+        }*/
+
+        /* static void Main(string[] args)
+         {
+             Console.ReadKey();
+             Console.WriteLine($"Start");
+
+             Thread thread1 = new Thread(()=>
+             {
+                 Thread thread2 = new Thread(() =>
+                 {
+                     Console.WriteLine($"Thread2 Start");
+                     Thread.Sleep(5000);
+                     Console.WriteLine($"Thread2 End");
+
+                 });
+                 Console.WriteLine($"Thread1 Start");
+                 thread2.Start();
+                 thread2.Join();
+
+                 Console.WriteLine($"Thread1 End");
+
+             });
+             Console.WriteLine($"Main Start");
+
+             thread1.Start();
+
+
+             Console.WriteLine($"Main End");
+
+         }
+
+         public static void Print()
+         {
+             Thread.Sleep(5000);
+             Console.WriteLine($"Second Thread");
+         }*/
+        /*static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.WriteLine($"Start");
+
+            Thread currentThread = Thread.CurrentThread;
+
+            Console.WriteLine(currentThread.Name);
+            Console.WriteLine(currentThread.ManagedThreadId);
+            Console.WriteLine(currentThread.Priority);
+            Console.WriteLine(currentThread.ThreadState);
+
+            new Thread(Thread1).Start(currentThread);
+
+            Console.WriteLine("Main end");
+
+
+        }
+        public static void Thread1(object? obj)
+        {
+            Thread.Sleep(2000);
+            Thread thread = obj as Thread;
+
+            Console.WriteLine(thread.ThreadState);
+        }*/
+        /*static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.WriteLine($"Start");
+
+            Thread thread1 = new Thread(Thread1);
+            Thread thread2 = new Thread(Thread2);
+            thread2.Start();
+            thread1.Start(thread2);
+
+        }
+
+        private static void Thread2(object? obj)
+        {
+            Thread.Sleep(5000);
+        }
+
+        public static void Thread1(object? obj)
+        {
+            Thread.Sleep(3000);
+            Thread thread = obj as Thread;
+
+            Console.WriteLine(thread.ThreadState);
+        }*/
+
+        /*static object locker = new object();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(Thread1);
+                thread.Name = $"Thread{i}";
+                Console.WriteLine($"Thread {i} start");
+                thread.Start();
+              
+
+            }
+        }
+
+        public static void Thread1()
+        {
+            lock (locker)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name} {i + 1}");
+                    Thread.Sleep(100);
+                }
+            }
+        }*/
+
+        //Task - не зависит от потока, в котором он был запущен.
+        /*static void Main(string[] args)
+            {
+                //Task task2 = new Task(() => { });
+                //Task task2 = Task.Run(() => { });
+                Task task = new Task(Thread1);
+                task.Start();
+
+                task.Wait();
+                Console.WriteLine("Main end");
+            }
+
+            public static void Thread1()
+            {
+                Console.WriteLine("Task1 start");
+                Thread.Sleep(1000);
+                Console.WriteLine("Task1 end");
+            }*/
+
+        //Массив Tasks
+        /*static void Main(string[] args)
+        {
+                
+            Task[] tasks = new Task[3];
+
+            for (int i = 0; i < tasks.Length; i++) 
+            {
+                tasks[i] = new Task(() =>
+                {
+                    Console.WriteLine($"Task {i} start");
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"Task {i} end");
+                });
+                tasks[i].Start();
+            }
+
+            Task.WaitAll(tasks); //ожидает все tasks
+            Console.WriteLine("Main end");
+            //tasks = null; обнуление потоков
+        }
+
+        public static void Thread1()
+        {
+            Console.WriteLine("Task1 start");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task1 end");
+        }*/
+
+        //мягкое завершение
+
+        /*static CancellationTokenSource tokenSource = new CancellationTokenSource();
+        static CancellationToken token = tokenSource.Token; 
+        static void Main(string[] args)
+        {
+                
+            Task task = new Task(Task1, token);
+            task.Start();
+
+            Thread.Sleep(500);
+            tokenSource.Cancel();
+
+            Console.WriteLine("Main end");
+
+            Console.WriteLine(task.Status); //статус
+        }
+
+        public static void Task1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine($"Task abort");
+                    return;
+                }
+                Console.WriteLine($"{i * i}");
+                Thread.Sleep(200); 
+               
+            }
+            
+        }*/
+
+        //ContinueWith
+        /*static void Main(string[] args)
+        {
+
+            Task task1 = new Task(Task1);
+            Task task2 = task1.ContinueWith(Task2);
+
+            task1.Start();
+            
+
+            Task.WaitAll(task1,task2); //ожидает все tasks
+            
+        }
+
+        public static void Task1()
+        {
+            Console.WriteLine("Task1 start");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task1 end");
+        }
+        public static void Task2(Task task)
+        {
+            Console.WriteLine("Task2 start");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task2 end");
+
+            Console.WriteLine($"\n\n{Task.CurrentId} {task.Id}");
+        */
+
+
+
+        /*static void Main(string[] args)
+        {
+
+            Task<int> task1 = new Task<int>(() => Sum(4, 5));
+            Task task2 = task1.ContinueWith(task => Print(task.Result));
+
+            task1.Start();
+
+            task2.Wait();
+
+
+
+            int Sum(int a, int b) => a + b;
+            void Print(int sum) => Console.WriteLine($"Sum: {sum}");
+        }
+
+        */
+    }
+
+}
+
+
+
+
